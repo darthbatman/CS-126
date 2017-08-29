3e993d9c1f6903eb32f23bb28d675f63081b4b43
Rishi Masand <rishimasand@wirelessprv-10-192-42-66.near.illinois.edu>
2017-08-29 15:01:18 -0500

    first commit

---
+++ src/bedazzle.java
@@ -0,0 +1,39 @@
+// Rishi Masand
+// University of Illinois at Urbana-Champaign
+// CS 126 Fall 2017
+// Lecture 1
+
+public class bedazzle {
+
+    public static String bedazzle(String input){
+
+        String b = "";
+
+        for (int i = 0; i < input.length(); i++){
+
+            b += input.charAt(i);
+
+            if (input.charAt(i) == ' '){
+
+                b += " ";
+
+            }
+            else {
+
+                b += "*";
+
+            }
+
+        }
+
+        return b;
+
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(bedazzle("This is my string"));
+
+    }
+
+}
