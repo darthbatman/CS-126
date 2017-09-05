c103b2701960cf4c1af71dc6b6eac9f4a006e522
Rishi Masand <rishimasand@wirelessprv-10-192-42-66.near.illinois.edu>
2017-09-05 14:11:54 -0500

    list format added

---
+++ src/ListFormat.java
@@ -0,0 +1,35 @@
+public class ListFormat {
+
+    public static String toString(String[] array) {
+
+        String formattedString = "";
+
+        for (int i = 0; i < array.length; i++){
+
+            formattedString += array[i];
+
+            if (i != array.length - 1){
+
+                formattedString += ", ";
+
+            }
+
+            if (i == array.length - 2){
+
+                formattedString += "and ";
+
+            }
+
+        }
+
+        return formattedString;
+
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(toString(new String[] { "thing1", "thing2", "thing3" }));
+
+    }
+
+}
--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -10,7 +10,9 @@ public class TicTacToe {
     public static int[][] winningSequences = { { 0, 1, 2 }, { 3, 4, 5 }, { 6, 7, 8 }, { 0, 3, 6 }, { 1, 4, 7 }, { 2, 5, 8 }, { 0, 4, 8 }, { 2, 4, 6 } };
 
     public enum Evaluation {
+
         InvalidInput, NoWinner, Xwins, Owins, UnreachableState
+
     }
 
     public static Evaluation EvaluateBoard(String board){
